b3fe8a55e86ad41fcdf6a75c610f378ad0ef1ff6
Antsukova Vladislava Maratovna <vlminsadyrova@ozon.ru>
2023-07-21 11:55:28 +0300

    add customer service

--- src/OrderService.CustomerService/Program.cs
+++ src/OrderService.CustomerService/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World OrderGenerator!");
+app.MapGet("/", () => "Hello World CustomerService!");
 
 app.Run();
